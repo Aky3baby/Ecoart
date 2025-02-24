ba9ed471f5483015363ea999777992bb6533d21e
linda <lindabrokye1993@gmail.com>
2025-02-24 12:35:33 +0000

    worked on nav

--- script.js
+++ script.js
@@ -5,3 +5,14 @@ const nav = document.querySelector('nav ul');
 menuToggle.addEventListener('click', () => {
   nav.classList.toggle('active');
 });
+
+
+document.querySelector('#about-link').addEventListener('click', function() {
+    document.querySelector('.about-section').classList.add('show');
+  });
+  function toggleMenu() {
+    const navLinks = document.querySelector('.nav-links');
+    navLinks.classList.toggle('active');
+  }
+
+  
